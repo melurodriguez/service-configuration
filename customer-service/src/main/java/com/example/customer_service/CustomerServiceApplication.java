7dde76c4c4e1d70c523e59d46a9eda1ad7be6736
Melina Rodriguez <melurod2003@gmail.com>
2026-07-22 20:48:23 -0300

    fix: .yml files to optimize access

--- customer-service/src/main/java/com/example/customer_service/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/customer_service/CustomerServiceApplication.java
@@ -10,11 +10,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-		dotenv.entries().forEach(entry ->
-						System.setProperty(entry.getKey(), entry.getValue())
-		);
-
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
