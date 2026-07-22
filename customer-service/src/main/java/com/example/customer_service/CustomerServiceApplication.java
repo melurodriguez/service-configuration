051d98d45754b3c2f5e4147926ddeacea154db71
Melina Rodriguez <melurod2003@gmail.com>
2026-07-22 20:34:54 -0300

    refactor: flatten nested service directory structure

--- customer-service/src/main/java/com/example/customer_service/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/customer_service/CustomerServiceApplication.java
@@ -1,5 +1,6 @@
 package com.example.customer_service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -9,6 +10,11 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry ->
+						System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
