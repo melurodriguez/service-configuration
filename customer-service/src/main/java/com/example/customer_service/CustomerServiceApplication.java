a381ec838dd23781d99e5c6b55533927e1d3df8c
Melina Rodriguez <melurod2003@gmail.com>
2026-07-26 22:56:07 -0300

    feature: add Swagger dependency

--- customer-service/src/main/java/com/example/customer_service/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/customer_service/CustomerServiceApplication.java
@@ -1,12 +1,18 @@
 package com.example.customer_service;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@OpenAPIDefinition(
+				info = @Info(title = "Customer Service", version = "1.0", description = "Customers app")
+)
+
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
--- product-service/src/main/java/com/example/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/com/example/product_service/ProductServiceApplication.java
@@ -1,10 +1,16 @@
 package com.example.product_service;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+				info = @Info(title = "Products Service", version = "1.0", description = "Products app")
+)
+
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
